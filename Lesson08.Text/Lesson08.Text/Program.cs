99f2d85deec2f558ace4eead757cc391c4de51e9
Serhii Olefir <senkaolefir@gmail.com>
2022-05-17 13:11:50 +0300

    lesson 09 text/enum

--- Lesson08.Text/Lesson08.Text/Program.cs
+++ Lesson08.Text/Lesson08.Text/Program.cs
@@ -18,11 +18,13 @@ namespace Lesson08.Text
             // tuple.a = 1;
             // int c = tuple.a + tuple.b;
 
+            // print file content
             foreach (var item in content)
             {
                 Console.WriteLine(item);
             }
 
+            // print deserialized data
             foreach ((string name, int number) item in Deserialize(content))
             {
                 Console.WriteLine($"Name is {item.name}, number is {item.number}");
@@ -30,15 +32,48 @@ namespace Lesson08.Text
 
             var phoneBook = Deserialize(content);
 
-            phoneBook[1].number = 101010101;
+            var newRecord = (name: "Nick", number: 1212);
+            Add(ref phoneBook, newRecord);
+            Update(phoneBook, newRecord, 3);
+            Delete(ref phoneBook, 0);
 
+            // print serialized data
             var serializedBook = Serialize(phoneBook);
             foreach (var item in serializedBook)
             {
                 Console.WriteLine(item);
             }
-            
+
             File.WriteAllLines(filePath, serializedBook);
+        }
+
+        private static void Add(ref (string name, int number)[] content, (string name, int number) newItem)
+        {
+            var newBook = new (string name, int number)[content.Length + 1];
+            content.CopyTo(newBook, 0);
+            newBook[content.Length] = newItem;
+            content = newBook;
+        }
+
+        private static void Update((string name, int number)[] content, (string name, int number) updatedItem, int index)
+        {
+            content[index] = updatedItem;
+        }
+
+        private static void Delete(ref (string name, int number)[] content, int index)
+        {
+            var newBook = new (string name, int number)[content.Length - 1];
+            int j = 0;
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (i != index)
+                {
+                    newBook[j++] = content[i];
+                }
+            }
+
+            content = newBook;
         }
 
         private static string[] Serialize((string name, int number)[] content)
@@ -54,7 +89,7 @@ namespace Lesson08.Text
 
         private static (string name, int number)[] Deserialize(string[] content)
         {
-            var regexp = new Regex(@"^(\w+);(\d+)$");
+            var regexp = new Regex(@"^(?<name>\w+);(?<number>\d+)$");
             var book = new (string name, int number)[content.Length];
             for (int i = 0; i < content.Length; i++)
             {
@@ -63,8 +98,8 @@ namespace Lesson08.Text
 
                 if (match.Success)
                 {
-                    book[i].name = match.Groups[1].Value;
-                    book[i].number = int.Parse(match.Groups[2].Value);
+                    book[i].name = match.Groups["name"].Value;
+                    book[i].number = int.Parse(match.Groups["number"].Value);
                 }
             }
             return book;
---
+++ Lesson09.Enums/Lesson09.Enums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson09.Enums
+{
+    enum DayOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek dayOfWeek = DayOfWeek.Monday;
+            foreach (DayOfWeek item in Enum.GetValues<DayOfWeek>())
+            {
+                Console.WriteLine((int)item);
+            }
+
+            DayOfWeek newDayOfWeek = (DayOfWeek)2;
+            Console.WriteLine($"Today is {newDayOfWeek}");
+            Console.WriteLine($"{dayOfWeek} is {GetHoliday(dayOfWeek)}");
+        }
+
+        private static string GetHoliday(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                    return "non-holiday";
+                case DayOfWeek.Sunday:
+                    return "holiday";
+            }
+
+            return string.Empty;
+        }
+    }
+}
