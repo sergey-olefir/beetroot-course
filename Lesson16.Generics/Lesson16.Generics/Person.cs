e107d41bf6a87967b85a4636ffd4b6ba0def9c10
Serhii Olefir <senkaolefir@gmail.com>
2022-06-04 11:53:55 +0300

    person commited

---
+++ Lesson16.Generics/Lesson16.Generics/Person.cs
@@ -0,0 +1,11 @@
+namespace Lesson16.Generics
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Age { get; set; }
+    }
+}
