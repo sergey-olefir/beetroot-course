68a6a6f5914e1fde29ffbf0f285a7e64025783ac
Serhii Olefir <senkaolefir@gmail.com>
2022-06-13 19:46:40 +0300

    lesson 20

---
+++ Lesson20.Extensions/Lesson20.Extensions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson20.Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "This is my string";
+            Console.WriteLine(WordCount(str1));
+            Console.WriteLine(str1.WordCount());
+
+            str1.WordCount();
+            str1.WordCount();
+
+            Console.WriteLine("sfsdfsdf".ToBool());
+            Console.WriteLine("true".ToBool());
+        }
+
+        public static int WordCount(string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?', '!' },
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
